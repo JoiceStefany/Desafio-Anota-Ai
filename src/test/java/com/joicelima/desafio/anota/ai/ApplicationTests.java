394a9bc94566c0e9a237a008efdd58f92c99755c
JoiceStefany <steffanyjoice006@gmail.com>
2024-02-26 15:36:50 -0300

    Mongo Config

---
+++ src/test/java/com/joicelima/desafio/anota/ai/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joicelima.desafio.anota.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
